d6da2a1d91c2935b6021fa2db556a54eb32e5502
thphuong <thphuong1093@gmail.com>
2022-11-08 15:46:06 +0700

    feat: separate ci workflows

--- generate.sh
+++ generate.sh
@@ -4,9 +4,14 @@ set -e
 
 source generate_utils.sh
 
+# Generate a script that can be used with `eval` to set meta variables from a tag
+#
+# Usage: meta_from_full_tag <full tag>
+# e.g. meta_from_full_tag 8.1.11-alpine3.15
+#
 meta_from_full_tag() {
     local php_version distro_release
-    IFS='-' read php_version distro_release <<< $1
+    IFS='-' read php_version distro_release <<< "$1"
 
     if contains "$distro_release" "$debian_releases"; then
         local distro=debian
@@ -23,8 +28,11 @@ local distro="$distro"
 EOF
 }
 
+# Generate dockerfile for a tag
+#
 # Usage: generate_dockerfile <full tag>
-# e.g. generate_dockerfile 7.4.29-alpine3.15
+# e.g. generate_dockerfile 8.1.9-alpine3.16
+#
 generate_dockerfile() {
     eval $(meta_from_full_tag $1)
 
@@ -38,7 +46,6 @@ generate_dockerfile() {
     write_warn_edit $dockerfile
 
     for tpl in $(ls Dockerfile*.template | grep -E "Dockerfile-[0-9]+(|-${variant})(-${distro})?.template"); do
-        # Base Dockerfile
         tpl "$tpl" \
             version=php_version \
             composer_version \
@@ -46,16 +53,16 @@ generate_dockerfile() {
             >> $dockerfile
     done
 
-    cp docker-php-ext-disable "$dir"
+    cp -r rootfs "$dir"
 }
 
+# Generate everything
+#
 generate_all() {
-    local distro_releases="$debian_releases $alpine_releases"
-
-    local targets=""
+    local targets
 
     for version in $(echo "$php_versions" | sed -e 's/ /\n/g' | sort); do
-        for distro in $distro_releases; do
+        for distro in $debian_releases $alpine_releases; do
             targets="$targets $version-$distro"
         done
     done
@@ -65,9 +72,14 @@ generate_all() {
     done
 
     generate_bake_file $targets
-    generate_workflow $targets
+
+    for version in $php_versions; do
+        generate_workflow $version
+    done
 }
 
+# Clean up generated files
+#
 clean_all() {
     for version in $php_versions; do
         rm -rf $(get_minor $version)
@@ -77,9 +89,13 @@ clean_all() {
     rm -f .github/workflows/ci.yml
 }
 
+# Generate all possible tags by performing matrix multiplication
+#
+# Usage: generate_tags <versions> <variants>
+# e.g. generate_tags "7.4.32 8.0.24 8.1.11" "bullseye buster"
+# would returns 7.4.32-bullseye 7.4.32-buster 8.0.24-bullseye 8.0.24-buster 8.1.11-bullseye 8.1.11-buster
+#
 generate_tags() {
-    local IFS=','
-
     local tags
 
     for version in $1; do
@@ -95,11 +111,24 @@ generate_tags() {
     echo "$tags"
 }
 
+# Normalize bake targets (replacing dots with underscores)
+#
 format_bake_target() {
     sed 's/\./_/g'
 }
 
+# Generate bake file targets for a tag
+# Includes major/minor version tag and short tags if PHP version or variant is the default one
+#
+# Usage: generate_bake_file_target <tag>
+# e.g. generate_bake_file_target 8.1.11-bullseye
+#
 generate_bake_file_target() {
+    local distro_release_var_name="default_${default_distro}_release"
+
+    local default_php_minor="$(get_minor $default_php_version)"
+    local default_distro_release=${!distro_release_var_name}
+
     eval $(meta_from_full_tag $1)
 
     [ "$php_minor" = "$default_php_minor" ] && local is_default_version=true
@@ -126,8 +155,8 @@ generate_bake_file_target() {
     fi
 
     tags=$(
-        generate_tags "$version_tags" "$distro_tags" \
-        | sed -E 's/(^|[[:space:]])/\1\\${REGISTRY}\/\\${REPO}:/g' \
+        IFS=',' generate_tags "$version_tags" "$distro_tags" \
+        | sed -E 's/(^|[[:space:]])/\1${REGISTRY}\/${REPO}:/g' \
         | format_list \
         | indent 1 4 \
         | trim
@@ -145,6 +174,8 @@ generate_bake_file_target() {
         tags
 }
 
+# Generate docker-bake.hcl file
+#
 generate_bake_file() {
     local bake_file="docker-bake.hcl"
 
@@ -161,27 +192,32 @@ generate_bake_file() {
     done
 }
 
+# Generate workflow file for the specified PHP version
+#
+# Usage: generate_workflow <version>
+# e.g. generate_workflow 8.1.11
+# Would generate the workflow file .github/workflows/8.1.yml
+#
 generate_workflow() {
-    local workflow_file=".github/workflows/ci.yml"
+    local php_minor=$(get_minor $1)
+    local targets="$(generate_tags $1 "$debian_releases $alpine_releases" | format_bake_target | format_list 2 | indent 5 2 | trim)"
+
+    local workflow_file=".github/workflows/$php_minor.yml"
 
     echo "generating $workflow_file ..."
 
     mkdir -p .github/workflows
     write_warn_edit $workflow_file
 
-    local targets=$(echo "$@" | format_bake_target | format_list 2 | indent 5 2 | trim)
     local platforms=$(echo $platforms | sed 's/ /,/g')
 
-    tpl ci.yml.template targets platforms >> $workflow_file
+    tpl ci.yml.template php_minor targets platforms >> $workflow_file
 }
-eval $(get_versions)
 
-default_php_major="$(get_major $default_php_version)"
-default_php_minor="$(get_minor $default_php_version)"
-eval default_distro_release=\$"default_${default_distro}_release"
-
-if [ "$#" = 0 ]; then
-    generate_all
-elif [ "$1" = "clean" ]; then
+if [ "$1" = "clean" ]; then
+    eval $(generate_config ${2:-versions.yml})
     clean_all
+else
+    eval $(generate_config ${1:-versions.yml})
+    generate_all
 fi
